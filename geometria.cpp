86f71d553772b8aef60248fbc058cc37b30bd757
Fabian camilo Yara mujica <Fabian.yara@Campusucc.edu.co>
2021-10-21 20:23:35 -0500

    se agrego la raiz del area

--- geometria.cpp
+++ geometria.cpp
@@ -1,38 +1,90 @@
-# include <stdio.h>
-# include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <iostream>
+
 using namespace std;
-int main()
-{{
-int lado = 5;
-int base = 3;
-int altura = 24;
-int radio = 15;
+
+int main() {
+        
+	int vi_opcion  = 1;
+	float vf_area  = 0;
+	bool vb_opcion = false;
+
+	int vi_base=0;
+        int vi_altura = 0;
+	int vi_radio = 0;
+        double b; 
+	cout << "+-------------------------------------------------------------+\n";
+	cout << "|   Programa para calcular el area de una figura geometrica   |\n";
+	cout << "|   Realizado por Fabian camilo yara Mujica                   |\n";
+	cout << "|   9/SEP  2021. Universidad Cooperativa de Colombia          |\n";
+	cout << "|   14/OCT 2021. rename and add fragment code                 |\n";
+	cout << "+-------------------------------------------------------------+\n";
+	cout << "\n";
+
+	cout << "Seleccionar una opcion geometrica para calcular el area\n";
+	cout << "Circulo   [1] por defecto\n";
+	cout << "Triangulo [2] \n";
+	cout << "Cuadrado  [3] \n";
+	cout << "[1] :";
+
+	cout << "Opcion por defecto = 1";
+	//cin >> vi_opcion;
+	vi_opcion = 1;
+
+	cout << "Usted ingreso: " << vi_opcion << endl;
+
+	if (vi_opcion < 1 ){
+		vi_opcion = 1;
+		vb_opcion = true;
+	}else if (vi_opcion > 3 ){
+		vi_opcion = 1;
+		vb_opcion = true;
+	}
+	if (vb_opcion) {
+		cout << "ingreso una opcion fuera de rango, se asigna por defecto 1\n";
+	}
+
+	switch(vi_opcion) {
+	  case 1:
+    	    // Circulo
+	    cout << "Ingrese el Radio del circulo : ";
+	    //cin >> vi_radio;
+	    vi_radio = 3;
+
+	    cout << "\n";
+	    vf_area = (vi_radio * vi_radio) * 3.14151692;
+	    break;
+	  case 2:
+	    // Triangulo
+	    cout << "Ingrese la altura : ";
+	    cin >> vi_altura;
+	    cout << "\n";
+
+	    cout << "Ingrese la base : ";
+            cin >> vi_base;
+            cout << "\n";
+
+	    vf_area = (vi_base * vi_altura) / 2;
+	    break;
+	  default:
+	    // Cuadrado
+	    cout << "Ingrese longitud de la base : ";
+            cin >> vi_base;
+            cout << "\n";
+
+	    vf_area = vi_base * vi_base;
+            break;
+	}
+
+	cout << "La area es => " << vf_area << endl; 
 
 
+	//Archivo Funcion
+	//a vf_area calcular raiz cuadrada en una variable e imprimir el resultado
+	b=sqrt(vf_area);
+	cout<< "la raiz cuadrada es "<<b<< endl;
 
-
-cout<<"EL AREA DEL CUADRADO ES:"<<lado*lado;
-
-
-
-cout<<"\nEL AREA DEL TRIANGULO ES:"<<base*altura/2;
-
-
-
-cout<<"\nEL AREA DEL CIRCULO ES:"<<3.1416*(radio*radio);
-
-
-
+	return 0;
 }
 
-
-
-cout <<"\n";
-cout <<"+--------------------------------------------------------+\n";
-cout <<"| programa para calcular el area de un figura geometrica |\n";
-cout <<"| realizado por Fabian camilo Yara mujica                |\n";
-cout <<"| 14/oct/2021 rename and add fragment code               |\n";
-cout <<"+--------------------------------------------------------+\n";
-cout <<"\n";
-return 0;
-}
